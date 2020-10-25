16923f7494fba180374b51860b950aa6318d23fe
songpol-at-pda-or-th <73422355+songpol-at-pda-or-th@users.noreply.github.com>
2020-10-25 16:57:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'WoLLQMekSIQM3HRRLp2jJJ2VzLhaDVNUz3HhVOmROiGQtiUtwSd9KX74PCZxaAOEc/4tIFSAbPGEka9StC95j65wDh8kVVdP5KjTrY6B0ujYGnUQG7f2JyM91BTjcQguQHHGfCufCcb7oMjy0ygIvgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
